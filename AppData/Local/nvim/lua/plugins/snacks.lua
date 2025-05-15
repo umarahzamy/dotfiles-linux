ece3264a05d9c948dc9525122439b4b4a66a5bc3
umarahzamy <134705653+umarahzamy@users.noreply.github.com>
2025-05-15 15:48:44 +0800

    feat(nvim): add explorer source configuration to snacks picker

--- AppData/Local/nvim/lua/plugins/snacks.lua
+++ AppData/Local/nvim/lua/plugins/snacks.lua
@@ -1,9 +1,17 @@
 return {
-	"folke/snacks.nvim",
-	opts = {
-		terminal = {
-			start_insert = false,
-			auto_insert = false,
-		},
-	},
+  "folke/snacks.nvim",
+  opts = {
+    terminal = {
+      start_insert = false,
+      auto_insert = false,
+    },
+    picker = {
+      sources = {
+        explorer = {
+          hidden = true,
+          ignored = true,
+        },
+      },
+    },
+  },
 }
