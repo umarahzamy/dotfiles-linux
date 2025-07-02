8cc1ef41bfbb4294c1c143a61719f9c813cdea26
umarahzamy <134705653+umarahzamy@users.noreply.github.com>
2025-07-02 14:37:21 +0800

    refactor(wezterm): add gui-attached handler and basic configuration

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,8 +1,46 @@
 local wezterm = require("wezterm")
-local config = {}
+local act = wezterm.action
+local mux = wezterm.mux
+local config = wezterm.config_builder()
 
--- Keep your other settings (font, shell, etc.)
+wezterm.on("gui-attached", function(domain)
+	-- maximize all displayed windows on startup
+	local workspace = mux.get_active_workspace()
+	for _, window in ipairs(mux.all_windows()) do
+		if window:get_workspace() == workspace then
+			window:gui_window():maximize()
+		end
+	end
+end)
+
+config.animation_fps = 60
+config.color_scheme = "Catppuccin Mocha"
 config.default_prog = { "pwsh.exe" }
 config.font = wezterm.font("JetBrains Mono")
-config.color_scheme = "Nord (base16)"
+config.font_size = 10.0
+config.leader = { key = "q", mods = "ALT", timeout_miliseconds = 2000 }
+config.keys = {
+    {
+        mods = "CTRL",
+        key = "t",
+        action = wezterm.action.SpawnTab "CurrentPaneDomain",
+    },	{
+		key = "w",
+		mods = "CTRL",
+		action = act.CloseCurrentTab({ confirm = true }),
+	},
+}
+config.line_height = 1.2
+config.max_fps =  60
+config.window_background_opacity = 0.5
+config.window_close_confirmation = "AlwaysPrompt"
+config.window_decorations = "RESIZE"
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+config.win32_system_backdrop = "Acrylic"
+
 return config
