a695bc44c975140e01b46ed0ab94da0dec5395bc
umarahzamy <134705653+umarahzamy@users.noreply.github.com>
2026-08-06 23:25:53 +0800

    fix: disable fff nvim autocomplete popup

--- .config/nvim/lua/plugins/fff.lua
+++ .config/nvim/lua/plugins/fff.lua
@@ -6,4 +6,12 @@ pcall(function()
   })
   vim.keymap.set("n", "<C-p>", function() require("fff").find_files() end, { desc = "fff: find files" })
   vim.keymap.set("n", "<C-S-f>", function() require("fff").live_grep() end, { desc = "fff: live grep" })
+
+  -- Disable autocomplete popup in fff buffers
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "fff_input" },
+    callback = function()
+      vim.opt_local.autocomplete = false
+    end,
+  })
 end)
